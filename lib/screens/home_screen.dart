bd138fb611c2333c8c13272456a9685812745749
Roberto Rivera <roberto_rivera09@hotmail.com>
2021-10-20 07:30:54 -0500

    documentos

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text(' '),
+      ),
+      bottomNavigationBar: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
